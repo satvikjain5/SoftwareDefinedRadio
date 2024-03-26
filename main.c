d020138bceb1dd8df6eece176a344e25aec01cc5
sattu905 <satvikjain5@gmail.com>
2024-03-27 00:55:00 +0530

    random

--- main.c
+++ main.c
@@ -1,6 +1,41 @@
-#include <stdio.h>
+/* Ground Penetrating Radar for bladeRF 2.0 micro*/
+#include <pthread.h>
+#include "common.h"
+#include <math.h>
 
-int main() {
-  printf("Hello World");
-  return 0;
+
+#ifdef USE_BLADERF
+#include <libbladeRF.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#include <math.h>
+#include <getopt.h>
+#include <unistd.h>
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+
+// ----------------------------------------------------------------some sys stuff----------------------------------------------------------------------------//
+#ifndef bool
+typedef int bool;
+#define true 1
+#define false 0
+#endif
+
+
+int gettimeofday(struct timeval *tv, void *ignored)
+{
+  FILENAME ft;
+  unsigned __int64 tmp = 0;
+  if (NULL != tv){
+    GetSystemTimeAsFileTime
+  }
 }
--- rx.c
+++ rx.c
@@ -1,10 +1,81 @@
 /* rx with metadata */
-
 #include <libbladeRF.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+#include <getopt.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <netinet/in.h>
+
+// ---------------------------------- some sys stuff ---------------------------------
+
+// ---------------------------------- some basic signal definition ------------------------------
+#define SAMPLE_PER_SYMBOL 4 //4Msps sample rate
+volatile int rx_buff_offset;
+#define LEN_BUF_IN_SAMPLE (4*4096)
+#define LEN_BUF (LEN_BUF_IN_SAMPLE * 2)
+#define LEN_BUF_IN_SYMBOL (LEN_BUF_IN_SAMPLE/SAMPLE_PER_SYMBOL)
+
+// ----------------------------------- bladeRF specific operation -------------------------------------
+char *boardname = "bladeRF";
+#define MAX_GAIN = 60
+#define DEFAULT_GAIN 45
+
+typedef struct bladerf_devinfo bladerf_devinfo;
+typedef struct bladerf bladerf_device;
+typedef int16_t IQ_TYPE;
+
+struct bladerf_async_task {
+  pthread_t rx_task;
+  pthread_mutex_t stderr_lock;
+};
+
+struct bladerf_data {
+  void                  **buffers;      /* Transmit Buffers */
+  size_t                num_buffers;     /* Number of buffers */
+  size_t                samples_per_buffer;    /* Samples per buffer */
+  unsigned int          idx;              /* Next sample that needs to go out */
+  volatile IQ_TYPE      *rx_buf;
+};
+
+
+struct bladerf_stream *stream;
+struct bladerf_async_task async_task;
+struct bladerf_data rx_data;
+
+
+void *stream_callback(struct bladerf *dev, struct bladerf_stream *stream,
+                      struct bladerf_metadata *metadata, void *samples,
+                      size_t num_samples, void *user_data)
+{
+  struct bladerf_data *my_data = (struct bladerf_data *)user_data;
+  size_t i;
+  int16_t *sample = (int16_t *)samples;
+  for (i = 0; i < num_samples; i++){
+    rx_buf[rx_buff_offset] = (((*sample) >> 4) & 0xFF);
+    rx_buff[rx_buf_offset+1] = (((*(sample+1)) >>  4) & 0xFF);
+    rx_buff_offset = (rx_buff_offset + 2) & (LEN_BUF - 1);
+    sample += 2;
+  }
+
+  if (do_exit) {
+    return NULL;
+  }
+  else {
+    void *rv = my_data->buffers[my_data->idx];
+    my_data->idx = (my_data->idx + 1) % my_data -> num_buffers;
+    return rv;
+  }
+}
+
 
 struct channel_config {
 bladerf_channel channel;
@@ -13,16 +84,17 @@ unsigned int bandwidth;
 unsigned int samplerate;
 int gain;
 };
+
+
 const unsigned samples_len = 10000;
 int16_t *rx_samples = NULL;
-usigned int 
-
+const unsigned int buffer_size = 8192;
+const unsigned int num_buffers = 16;
+const unsigned int num_transfers = 8;
+const unsigned int timeout_ms = 3500;
+const unsigned int samplerate = 2.4e6;
 static int init_sync_rx(struct bladerf *dev){
   int status;
-  const unsigned int num_buffers = 16;
-  const unsigned int buffer_size = 8192;
-  const unsigned num_transfers = 8;
-  const unsigned int timeout_ms = 3500;
 
   status = bladerf_sync_config(dev, BLADERF_RX_X2, BLADERF_FORMAT_SC16_Q11, num_buffers, buffer_size, num_transfers, timeout_ms);
   if (status != 0){
@@ -31,14 +103,14 @@ static int init_sync_rx(struct bladerf *dev){
   }
   else {
     printf("Successfully configured RX sync interface!\n");
-  }
+  
   return status;
+}
 }
 
 int sync_rx_meta_schedule(struct bladerf *dev, int16_t *samples,
                           unsigned int samples_len,
                           unsigned int rx_count,
-                          unsigned int samplerate,
                           unsigned int timeout_ms){
   int status;
   struct bladerf_metadata meta;
@@ -107,8 +179,8 @@ int main(int argc, char* argv[])
     return 1;
   }
   init_sync_rx(dev);
-
-
+  sync_rx_meta_schedule(dev, rx_samples, samples_len,
+                        num_transfers, timeout_ms);
 }
 
 
